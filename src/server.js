6b1f276a3fa5115fa213c45eb164949a2de3d87b
BuildTools <unconfigured@null.spigotmc.org>
2024-04-28 15:15:15 -0600

    Working website

--- src/server.js
+++ src/server.js
@@ -11,14 +11,9 @@ app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/index')
-app.get('/MerisViews', function (req, res) {
-  console.log('GET called')
-  res.render('Meris')
-})
-
-app.get('/MerisViews', function (req, res) {
-  console.log('GET called')
+app.get('index')
+app.get('/views', function (req, res) {
+  console.log('/views')
   res.render('index')
 })
 
@@ -26,16 +21,3 @@ app.get('/MerisViews', function (req, res) {
 app.listen(3000, function () {
   console.log('Listening on port 3000...')
 })
-
-
-app.post('/', function (req,res){
-    console.log('adding todo item')
-    const stmt = db.prepare('INSERT INTO todo (task) VALUES (?)')
-    stmt.run(req.body.task)
-    stmt.finalize()
-  })
-
-  db.run(`CREATE TABLE todo (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    task TEXT NOT NULL)`)
-  
