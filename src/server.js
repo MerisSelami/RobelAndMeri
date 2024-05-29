af10d00c2fcc30ca5761b08fb35ccc46a10f58fb
Robel <77524608+Robie7707@users.noreply.github.com>
2024-05-28 21:59:16 -0600

    fixed the issue with the about page not loading(it was not initiated in the server.js file).

--- src/server.js
+++ src/server.js
@@ -37,6 +37,11 @@ app.get('/future', function (req, res) {
   res.render('future')
 })
 
+app.get('/about', function (req, res) {
+  console.log('CALLED about')
+  res.render('about')
+})
+
 // Start the web server
 app.listen(3000, function () {
   console.log('Listening on port 3000...')
